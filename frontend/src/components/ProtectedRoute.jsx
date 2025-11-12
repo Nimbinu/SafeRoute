066eaabf138731e6ef3d8803ffab73aeca036599
Nimbinu <164753328+Nimbinu@users.noreply.github.com>
2025-11-12 10:39:44 +0530

    Create ProtectedRoute.jsx

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,7 @@
+// TODO: Implement ProtectedRoute component
+import { Navigate } from 'react-router-dom';
+
+export default function ProtectedRoute({ children }) {
+  // TODO: Check authentication
+  return children;
+}
