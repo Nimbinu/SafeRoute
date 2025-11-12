48ee8aa311c44dc622d2f6cdc1233dc58700946c
Nimbinu <164753328+Nimbinu@users.noreply.github.com>
2025-11-12 10:40:28 +0530

    Create ReportModal.jsx

---
+++ frontend/src/components/ReportModal.jsx
@@ -0,0 +1,10 @@
+// TODO: Implement ReportModal component
+export default function ReportModal({ isOpen, onClose }) {
+  if (!isOpen) return null;
+  
+  return (
+    <div className="modal">
+      {/* TODO: Add modal content */}
+    </div>
+  );
+}
